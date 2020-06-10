3dd79ec88e43f754b5ba20986e545b2ce47e2a03
Nicolayqa <Nicolayqa@gmail.com>
2020-06-10 18:11:23 +0300

    ttf12

---
+++ src/test/java/com/OnlineDivorce/TestSuite_Registration.java
@@ -0,0 +1,12 @@
+package com.OnlineDivorce;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        Qualify_Form.class
+})
+
+public class TestSuite_Registration {
+}
